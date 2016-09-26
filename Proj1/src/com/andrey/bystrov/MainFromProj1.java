f16c513f0170e96c4f083a7c7b2c3404679eb0cf
andrey.bystrov <andrey.bystrov@exactprosystems.com>
2016-09-26 16:11:41 +0300

    asd

---
+++ Proj1/src/com/andrey/bystrov/MainFromProj1.java
@@ -0,0 +1,13 @@
+package com.andrey.bystrov;
+
+public class MainFromProj1 {
+	private String str;
+	
+	public MainFromProj1(String str) {
+		this.str = str;
+	}
+
+	public String getStr() {
+		return str;
+	}
+}
---
+++ Proj2/src/com/andrey/MainFromProj2.java
@@ -0,0 +1,9 @@
+package com.andrey;
+
+import com.andrey.bystrov;
+
+public class MainFromProj2 {
+	public static void maint(String ...args) {
+		System.out.println(new MainFromProj1("my string"));
+	}
+}
